bb70b92cc79d026dc18c967a88ef1120dabbd516
Sparsh0508 <https://github.com/Sparsh0508>
2025-09-14 21:41:47 +0530

    local host change

--- backend/Socket/Socket.js
+++ backend/Socket/Socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:['http://localhost:5173'],
+        origin:['https://chatlify-4x5z.onrender.com'],
         methods:["GET","POST"]
     }
 });
