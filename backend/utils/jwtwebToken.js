a0de28a57cac5e28e476df4f27731cd75aa521cf
Sparsh0508 <https://github.com/Sparsh0508>
2025-09-14 21:30:50 +0530

    change secure

--- backend/utils/jwtwebToken.js
+++ backend/utils/jwtwebToken.js
@@ -8,7 +8,7 @@ const jwtToken = (userId,res) => {
     res.cookie('jwt',token,{
         maxAge: 30*24*60*60*1000,
         httpOnly: true,
-        secure: process.env.SECURE !== 'development',
+        secure: true,
         sameSite: 'strict',
     })
 }
